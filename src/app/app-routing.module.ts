45422bfc56f3f33e09472841dc55a2744d5d3995
Rares Mihail <bozoanca.rares.mihail@gmail.com>
2023-08-03 18:02:22 +0300

    Added Navigation bar and Blog page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { BlogPageComponent } from './components/blog-page/blog-page.component';
+
+const routes: Routes = [
+  { path: 'home', component: LandingPageComponent },
+  { path: 'blog', component: BlogPageComponent },
+  { path: '',   redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { BlogPageComponent } from './components/blog-page/blog-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    NavBarComponent,
+    BlogPageComponent
   ],
   imports: [
     BrowserModule,
